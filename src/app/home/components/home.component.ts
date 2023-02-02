c807cef5dac85bbf627d4ba8f24ce2fc8c82f6b4
roibar81 <baraziroi@gmail.com>
2023-02-02 21:05:11 +0200

    edit login page

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent {
-
+    constructor(){}
+    ngOnInit(): void {
+      //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+      //Add 'implements OnInit' to the class.
+      
+    }
 }
